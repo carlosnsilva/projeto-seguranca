d9a2ae31fa1d2d13bb93ae17e8b8f436af9791f3
Jorge Ricardo Soares Abrantes Alencar de Souza <jorge.souza@conductor.com.br>
2020-12-14 17:15:21 -0300

    adicionando google e refatorando estrutura

--- src/main/resources/static/main.js
+++
@@ -1,8 +0,0 @@
-var logout = function () {
-    $ .post ("/ logout", function () {
-        $ ("# usuário"). html ('');
-        $ (". não autenticado"). show ();
-        $ (". authenticated"). hide ();
-    })
-    return true;
-}
